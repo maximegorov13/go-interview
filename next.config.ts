77e1bccf99e15d51046ea4ba28e5e17f4be08071
Maxim Egorov <maximegorov1998@yandex.ru>
2025-11-18 01:53:33 +0500

    test github actions

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import path from 'node:path';
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
-  unstable_shouldAddLocaleToLinks: true,
 });
 
 // Export the final Next.js config with Nextra included
