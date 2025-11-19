9fa8a4796a2723b579e0ff2baece7c7365fc91ae
Maxim Egorov <maximegorov1998@yandex.ru>
2025-11-20 00:53:59 +0500

    Сформирована примерная навигация

---
+++ app/_meta.global.ts
@@ -0,0 +1,20 @@
+import type { MetaRecord } from 'nextra';
+
+const GLOBAL: MetaRecord = {
+  index: '',
+  separator_1: {
+    type: 'separator',
+    title: 'Go',
+  },
+  slices: {
+    items: {
+      tasks: '',
+    },
+    theme: {
+      collapsed: true,
+    },
+  },
+  maps: '',
+};
+
+export default GLOBAL;
