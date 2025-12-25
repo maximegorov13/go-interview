5ae2970067982be5402d0f453f36cbf16eef7aa6
Maxim Egorov <maximegorov1998@yandex.ru>
2025-12-25 14:34:39 +0500

    feat: добавлены разделы по продвинутому go, расширена общая навигация

--- app/_meta.global.ts
+++ app/_meta.global.ts
@@ -26,13 +26,45 @@ const GLOBAL: MetaRecord = {
       context: '',
       'array-slice': '',
       maps: '',
+      defer: '',
+      error: '',
+      concurrency: '',
+      'runtime-scheduler': '',
+      'garbage-collector': '',
     },
   },
   separator_2: {
     type: 'separator',
+    title: 'Базы данных',
+  },
+  db: {
+    title: 'Базы данных',
+    display: 'children',
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    items: {
+      'sql-nosql': '',
+    },
+  },
+  separator_3: {
+    type: 'separator',
+    title: 'Общее',
+  },
+  common: {
+    title: 'Общее',
+    display: 'children',
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    items: {
+      networks: '',
+    },
+  },
+  separator_4: {
+    type: 'separator',
     title: 'Задачи',
   },
   tasks: {
+    title: 'Задачи',
     display: 'children',
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
